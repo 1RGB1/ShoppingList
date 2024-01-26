7ca85ca61243a118b6e921157f4e806130903a95
1RGB1 <a.ragab2012@gmail.com>
2024-01-26 08:49:35 +0300

    Organizing project folders

---
+++ ShoppingList/ShoppingList/SupportingFiles/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  ShoppingList
+//
+//  Created by Ahmad Ragab on 26/01/2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
